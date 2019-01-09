d8590caf44e44230318831c1a474c7364b7a8516
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-09 11:35:33 +0000

    Basic Campaign RSpec test

--- spec/models/campaigns_spec.rb
+++ spec/models/campaigns_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @subject = Campaign.new(name: 'Some campaign',
+                            image: 'some image',
+                            percent_raised: 50,
+                            target_amount: 100,
+                            sector: 'Charity',
+                            country: 'UK',
+                            invest_multiple: 5)
+  end
+
+  it 'shows the campaign name' do
+    expect(@subject.name).to eq('Some campaign')
+  end
 end
