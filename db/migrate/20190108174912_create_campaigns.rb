013a1c73f4fce8bf1b9cc8c81a4b65ff00f0a8ca
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-09 10:14:33 +0000

    Setup API & introduced versioning

---
+++ db/migrate/20190108174912_create_campaigns.rb
@@ -0,0 +1,15 @@
+class CreateCampaigns < ActiveRecord::Migration[5.2]
+  def change
+    create_table :campaigns do |t|
+      t.string :name
+      t.string :image
+      t.integer :percent_raised
+      t.integer :target_amount
+      t.string :sector
+      t.string :country
+      t.integer :invest_multiple
+
+      t.timestamps
+    end
+  end
+end
