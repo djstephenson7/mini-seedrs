2ca642de23ee1c573b4d393b4500f5eeb5fbe843
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-11 10:52:14 +0000

    Added test for investment multiple

--- spec/features/investment_multiple_spec.rb
+++ spec/features/investment_multiple_spec.rb
@@ -10,4 +10,13 @@ describe 'Make the user stick to the investment multiple', type: :feature do
     click_button 'Update Campaign'
     expect(page).to have_content(30)
   end
+
+  it 'should have an investment box' do
+    visit '/campaigns/3'
+    click_link 'Invest'
+    fill_in 'campaign[amount_raised]', with: 31
+    click_button 'Update Campaign'
+    expect { page }
+      .to raise_error('Amount must be within the investment multiple')
+  end
 end
